498e286ecdc835feec90d670ecba674a2d912d78
j-pai <jesse.pai@gmail.com>
2018-02-26 21:46:24 -0500

    Some documentation about the scale_factor

--- daisy_brain.py
+++ daisy_brain.py
@@ -13,6 +13,11 @@ faces = {
 tracker_types = ["BOOSTING", "MIL", "KCF", "TLD", "MEDIANFLOW", "GOTURN", "MOSSE"]
 
 if __name__ == "__main__":
+    """
+    Dropping the scale factor from 1 means that the face needs to be closer to
+    the camera. Please make sure the scale factor is greater than 0 and less than
+    or equal to 1.
+    """
     daisy_test.identify_person(faces, "JessePai", scale_factor = 1)
     #daisy_test.track_object(tracker_types[6])
     #daisy_test.track_object_all_types()
