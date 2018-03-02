880eccf2c0eb730ba527342d6ee0ee46f392d824
j-pai <jesse.pai@gmail.com>
2018-03-02 17:29:51 -0500

    Removed unnecessary code

--- daisy_brain.py
+++ daisy_brain.py
@@ -16,12 +16,12 @@ tracker_types = ["BOOSTING", "MIL", "KCF", "TLD", "MEDIANFLOW", "GOTURN", "MOSSE
 tracker_colors = [(0, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255), (255, 0, 255), (255, 255, 255)]
 
 if __name__ == "__main__":
-    spine = DaisySpine()
+    #spine = DaisySpine()
     """
     Dropping the scale factor from 1 means that the face needs to be closer to
     the camera. Please make sure the scale factor is greater than 0 and less than
     or equal to 1.
     """
     #daisy_test.identify_person(faces, scale_factor = 1)
-    #daisy_test.track_object_all_types(types = ["CSRT"])
+    daisy_test.track_object_all_types(types = ["CSRT"])
     #bbox = daisy_test.id_and_track_face(faces, "JessePai")
