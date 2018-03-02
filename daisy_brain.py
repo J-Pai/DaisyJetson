13717904e66760b5762539d47906942d2343b52b
j-pai <jesse.pai@gmail.com>
2018-03-02 16:36:55 -0500

    Updated spine with more robust serial flushing and data passing.

--- daisy_brain.py
+++ daisy_brain.py
@@ -4,7 +4,7 @@ import face_recognition
 import cv2
 import daisy_test
 import daisy_eye
-import daisy_spine
+from daisy_spine import DaisySpine
 
 faces = {
     "JessePai": "./faces/JPai-1.jpg",
@@ -16,11 +16,12 @@ tracker_types = ["BOOSTING", "MIL", "KCF", "TLD", "MEDIANFLOW", "GOTURN", "MOSSE
 tracker_colors = [(0, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255), (255, 0, 255), (255, 255, 255)]
 
 if __name__ == "__main__":
+    spine = DaisySpine()
     """
     Dropping the scale factor from 1 means that the face needs to be closer to
     the camera. Please make sure the scale factor is greater than 0 and less than
     or equal to 1.
     """
-    daisy_test.identify_person(faces, scale_factor = 1)
+    #daisy_test.identify_person(faces, scale_factor = 1)
     #daisy_test.track_object_all_types(types = ["CSRT"])
     #bbox = daisy_test.id_and_track_face(faces, "JessePai")
--- daisy_spine.py
+++ daisy_spine.py
@@ -3,25 +3,83 @@ from sys import argv
 
 class DaisySpine:
     ser = None
-    def __init__(self, com_port = "/dev/ttyACM0", baud_rate = 9600, time_out = 1):
+    def __init__(self, com_port = "/dev/ttyACM1", baud_rate = 28800, time_out = 1):
         self.ser = serial.Serial(com_port, baud_rate, timeout = time_out)
 
     def read_line(self):
         print(self.ser.readline())
 
-    def read_all_lines(self):
-        while self.ser.inWaiting() > 0:
-            print(self.ser.readline())
-        self.ser.flushInput()
+    def read_all_lines_debug(self, chunk_size = 200):
+        print("=== READING ALL LINES ===")
+
+        read_buffer = b''
+
+        while True:
+            byte_chunk = self.ser.read(size = chunk_size)
+            read_buffer += byte_chunk
+            if not len(byte_chunk) == chunk_size:
+                break
+
+        print(read_buffer.decode("utf-8"))
+        self.ser.reset_input_buffer()
+
+        return read_buffer;
+
+        print("=== DONE ===")
+
+    def read_all_lines(self, chunk_size = 50):
+        read_buffer = b''
+
+        while True:
+            byte_chunk = self.ser.read(size = chunk_size)
+            read_buffer += byte_chunk
+            if not len(byte_chunk) == chunk_size:
+                break
+
+        self.ser.reset_input_buffer()
+
+        return read_buffer;
 
     def pass_byte_basic(self, b):
         self.ser.write(bytes([int(b)]))
 
-    def pass_byte(self, b):
-        self.ser.flushInput()
+    def pass_byte_debug(self, b):
+        print("+++ PASSING BYTE +++")
+
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
+
+        if (int(b) > 255 or int(b) < 0):
+            print("Byte out of range: " + b)
+            print("+++ FAIL +++")
+            return
+
         print("Passing byte " + str(b))
         self.pass_byte_basic(b)
-        print(self.ser.readline())
+
+        ret = self.read_all_lines()
+
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
+
+        print("+++ DONE +++")
+        return ret
+
+    def pass_byte(self, b):
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
+
+        if (int(b) > 255 or int(b) < 0):
+            print("Byte out of range: " + b)
+            return
+
+        self.pass_byte_basic(b)
+        ret = self.read_all_lines()
+
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
+
+        return ret
 
     def forward(self):
         print("Forward")
@@ -50,10 +108,10 @@ if __name__ == "__main__":
     else:
         spine = DaisySpine()
 
-    spine.read_all_lines()
+    print(spine.read_all_lines())
     spine.halt();
     while True:
         # spine.forward()
         input_str = input()
         if (len(input_str) > 0):
-            spine.pass_byte(input_str)
+            print(spine.pass_byte(input_str))
