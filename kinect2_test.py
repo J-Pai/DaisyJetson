b431a94905b75de165994a04ee2fd6b25308488c
j-pai <jesse.pai@gmail.com>
2018-03-24 21:02:25 -0400

    Added tracker to kinect stream.

--- kinect2_test.py
+++ kinect2_test.py
@@ -2,10 +2,12 @@
 
 import numpy as np
 import cv2
-import dlib
 import sys
 from pylibfreenect2 import Freenect2, SyncMultiFrameListener
 from pylibfreenect2 import FrameType, Registration, Frame
+from pylibfreenect2 import setGlobalLogger
+
+setGlobalLogger(None)
 
 try:
     print("OpenGL Pipeline")
@@ -24,11 +26,10 @@ except:
 RGB_W = 1920
 RGB_H = 1080
 
-DEPTH_W = 1512
-DEPTH_H = 1080
-
-depth_view = (int(RGB_W/2) - int(DEPTH_W/2), int(RGB_H/2) - int(DEPTH_H/2),
-        int(RGB_W/2) + int(DEPTH_W/2), int(RGB_H/2) + int(DEPTH_H/2))
+DEFAULT_FACE_TARGET_BOX = (int(RGB_W/2) - 75, int(RGB_H/2) - 100,
+        int(RGB_W/2) + 75, int(RGB_H/2) + 100)
+DEFAULT_TRACK_TARGET_BOX = (int(RGB_W/2) - 340, int(RGB_H/2) - 220,
+        int(RGB_W/2) + 340, int(RGB_H/2) + 220)
 
 def __draw_bbox(valid, frame, bbox, color, text):
     if not valid:
@@ -37,7 +38,7 @@ def __draw_bbox(valid, frame, bbox, color, text):
     cv2.putText(frame, text, (bbox[0], bbox[1] - 4), \
             cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
 
-def __init_tracker(self, frame, bbox, tracker_type = "BOOSTING"):
+def __init_tracker(frame, bbox, tracker_type = "BOOSTING"):
     tracker = None;
 
     print("Init Tracker with:", bbox, tracker_type)
@@ -58,11 +59,6 @@ def __init_tracker(self, frame, bbox, tracker_type = "BOOSTING"):
         tracker = cv2.TrackerMOSSE_create()
     if tracker_type == "CSRT":
         tracker = cv2.TrackerCSRT_create()
-    if tracker_type == "DLIB":
-        tracker = dlib.correlation_tracker()
-        tracker.start_track(frame, \
-                dlib.rectangle(bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]))
-        return tracker
 
     ret = tracker.init(frame, bbox)
 
@@ -70,7 +66,7 @@ def __init_tracker(self, frame, bbox, tracker_type = "BOOSTING"):
         return None
     return tracker
 
-def __select_ROI(self, frame):
+def __select_ROI(frame):
     bbox = cv2.selectROI(frame, False)
     cv2.destroyAllWindows()
     return bbox;
@@ -79,7 +75,11 @@ def clean_color(color, bigdepth, min_range, max_range):
     #__draw_bbox(True, color, depth_view, (0, 255, 0), "depth_view")a
     color[(bigdepth < min_range) | (bigdepth > max_range)] = 0
 
-def view_kinect(tracker = "CSRT", min_range = 1000, max_range = 2000):
+def view_kinect(tracker = "CSRT", min_range = 0, max_range = 1000,
+        track_target_box = DEFAULT_TRACK_TARGET_BOX,
+        face_target_box = DEFAULT_FACE_TARGET_BOX,
+        res = (RGB_W, RGB_H),
+        video_out = True, debug = True):
     fn = Freenect2()
     num_devices = fn.enumerateDevices()
 
@@ -103,7 +103,18 @@ def view_kinect(tracker = "CSRT", min_range = 1000, max_range = 2000):
     registered = Frame(512, 424, 4)
     bigdepth = Frame(1920, 1082, 4)
 
+    initialized = False
+
+    trackerObj = None
+    face_count = 5
+    face_process_frame = True
+
+    bbox = None
+    face_bbox = None
+    track_bbox = None
+
     while True:
+        timer = cv2.getTickCount()
         frames = listener.waitForNewFrame()
 
         color = frames["color"]
@@ -111,14 +122,42 @@ def view_kinect(tracker = "CSRT", min_range = 1000, max_range = 2000):
 
         registration.apply(color, depth, undistorted, registered, bigdepth=bigdepth)
 
-        convertedBigDepth = np.resize(bigdepth.asarray(np.float32), (1080, 1920))
-        convertedColor = cv2.cvtColor(color.asarray(), cv2.COLOR_RGB2BGR)
-        convertedRegis = cv2.cvtColor(registered.asarray(np.uint8), cv2.COLOR_RGB2BGR)
+        bd = np.resize(bigdepth.asarray(np.float32), (1080, 1920))
+        c = cv2.cvtColor(color.asarray(), cv2.COLOR_RGB2BGR)
 
-        clean_color(convertedColor, convertedBigDepth, min_range, max_range)
+        # clean_color(c, bd, min_range, max_range)
 
-        cv2.imshow("color", cv2.resize(convertedColor, (int(1920 / 3), int(1080 / 3))))
-        #cv2.imshow("bigdepth", cv2.resize(convertedBigDepth / 4500, (int(1920/ 3), int(1080 / 3))))
+        if not initialized:
+            bbox = __select_ROI(c)
+            trackerObj = __init_tracker(c, bbox, tracker)
+            initialized = True
+
+        status = False
+
+        if trackerObj is not None:
+            status, trackerBBox = trackerObj.update(c)
+            bbox = (int(trackerBBox[0]),
+                    int(trackerBBox[1]),
+                    int(trackerBBox[0] + trackerBBox[2]),
+                    int(trackerBBox[1] + trackerBBox[3]))
+
+        if bbox is not None:
+            track_bbox = bbox
+
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+
+        if video_out:
+            output_frame = c.copy()
+            __draw_bbox(status, output_frame, track_bbox, (0, 255, 0), tracker)
+            cv2.putText(output_frame, "FPS : " + str(int(fps)), (100,50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,255), 1)
+            if not status:
+                failedTrackers = "FAILED: "
+                failedTrackers += tracker + " "
+                cv2.putText(output_frame, failedTrackers, (100, 80),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,142), 1)
+
+            cv2.imshow("color", cv2.resize(output_frame, (int(1920 / 2), int(1080 / 2))))
 
         listener.release(frames)
 
